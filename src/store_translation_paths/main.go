6017e764882262431480df21cadaa6223aa2f2ac
bodrovis <bodrovis@protonmail.com>
2024-12-30 20:50:38 +0200

    bump [skip ci]

--- src/store_translation_paths/main.go
+++ src/store_translation_paths/main.go
@@ -21,7 +21,7 @@ func main() {
 	fmt.Fprintln(os.Stderr, "Debug: raw FLAT_NAMING =", rawFlatNaming)
 
 	// Parse flat naming
-	flatNaming, err := parsers.ParseBoolEnv(os.Getenv("FLAT_NAMING"))
+	flatNaming, err := parsers.ParseBoolEnv("FLAT_NAMING")
 	if err != nil {
 		returnWithError("invalid value for FLAT_NAMING environment variable; expected true or false")
 	}
